694057a662e827ba05b8b78ff1df097998e6c920
Vishal Das <vdas53073@gmail.com>
2023-05-22 01:58:31 +0530

    1.0

--- Backend/app.js
+++ Backend/app.js
@@ -3,16 +3,21 @@ import mongoose from "mongoose";
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from "cors";
+import dotenv from 'dotenv'
+
 const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
+
 mongoose
   .connect(
     "mongodb+srv://vdas04:databasevd04@cluster0.fsxqvgj.mongodb.net/?retryWrites=true&w=majority"
   )
-  .then(() => app.listen(5000))
+  .then(() => app.listen(port))
   .then(() =>
     console.log("Connected TO Database and Listening TO Localhost 5000")
   )
